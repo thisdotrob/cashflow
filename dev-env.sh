eff0cc59af8d247b0d1e93c957d637ff13c405bc
thisdotrob <rs@gower.st>
2018-08-13 09:57:21 +0100

    Remove unused env files, update npm scripts

--- dev-env.sh
+++
@@ -1,5 +0,0 @@
-export PORT=3000
-export STARLING_HOST=localhost:4000
-export STARLING_TOKEN=fakestarlingtoken
-export MONTHLY_AMOUNTS_FILENAME=monthly-amounts-stub.json
-export AMEX_TRANSACTIONS_FILENAME=amex-transactions-stub.csv
